3bd960a0d7d97cf3905b437070db6802cc7ac795
mohit701473 <mohitprajapat7014@gmail.com>
2023-04-13 16:18:45 +0530

    web d

--- Udemy Web-D Course/Section 18 Node.js/index.js
+++ Udemy Web-D Course/Section 18 Node.js/index.js
@@ -1,3 +1,12 @@
-const fs =  require('fs') ;
+// const fs =  require('fs') ;
 
-fs.copyFileSync("file1.txt", "file2.txt") ;
+// fs.copyFileSync("file1.txt", "file2.txt") ;
+
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var mySuperHeroName = superheroes.random() ;
+var mySuperVillainName = supervillains.random() ;
+
+console.log("My super hero is " + mySuperHeroName) ;
+console.log("My super villian is " + mySuperVillainName) ;
