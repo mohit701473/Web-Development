25fb48a20c2eae7afd50b0623f97e79aaef71d2b
mohit701473 <mohitprajapat7014@gmail.com>
2023-04-15 11:42:02 +0530

    express

---
+++ Udemy Web-D Course/Section 25 Express.js with Node.js/my-express-server/server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express() ;
+
+app.get("/", function(req, res){
+    //console.log(request) 
+    res.send("<h1>Hello</h1>")
+})
+
+app.listen(3000, function(){
+    console.log("server started on port 3000") ;
+}) ;
