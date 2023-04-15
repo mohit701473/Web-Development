06dd908ff318a22d9a82a9edec212ae34e83bec3
mohit701473 <mohitprajapat7014@gmail.com>
2023-04-15 14:15:44 +0530

    Update server.js

--- Udemy Web-D Course/Section 25 Express.js with Node.js/my-express-server/server.js
+++ Udemy Web-D Course/Section 25 Express.js with Node.js/my-express-server/server.js
@@ -2,8 +2,19 @@ const express = require("express")
 const app = express() ;
 
 app.get("/", function(req, res){
-    //console.log(request) 
     res.send("<h1>Hello</h1>")
+})
+
+app.get("/contact", function(req, res){
+    res.send("contact me at: mohit@gmail.com")
+})
+
+app.get("/about", function(req, res){
+    res.send("about me")
+})
+
+app.get("/work", function(req, res){
+    res.send("Here is my working profile")
 })
 
 app.listen(3000, function(){
