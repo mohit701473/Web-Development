aba43bcf78d68e93e28e961d4c2d78b396807338
mohit701473 <mohitprajapat7014@gmail.com>
2023-04-16 11:46:14 +0530

    k

---
+++ Udemy Web-D Course/Section 25 Express.js with Node.js/Calculator/calculator.js
@@ -0,0 +1,15 @@
+const express = require("express") 
+
+const app = express() 
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname +"\\index.html")
+})
+
+app.post("/", function(req, res){
+    res.send("thanks for posting") 
+})
+
+app.listen(3000, function(){
+    console.log("server is running on port 3000")
+})
