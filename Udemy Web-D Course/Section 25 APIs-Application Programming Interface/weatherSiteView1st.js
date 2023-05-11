320facea067147ac80a4aab4d00e2e7c97f5cb4f
mohit701473 <mohitprajapat7014@gmail.com>
2023-05-11 12:45:09 +0530

    Weather App

--- Udemy Web-D Course/Section 25 APIs-Application Programming Interface/weatherSiteView1st.js
+++ Udemy Web-D Course/Section 25 APIs-Application Programming Interface/weatherSiteView1st.js
@@ -1,48 +1,38 @@
-const express = require("express")  
+const express = require("express")  ;
 const https = require("https")
-const bodyParser = require("body-parser")
 
 const app = express() ;
 
-app.use(bodyParser.urlencoded({extended: true}))
-
 app.get("/", function(req,res){
-    
-    res.sendFile(__dirname + "//weather.html") 
-})
 
-app.post("/", function(req, res){
-    console.log(req.body.cityName)
-    console.log("post request recived")
-
-    const query = req.body.cityName
-    const apiKey = "e11166873fa5b9137dea09cd24f7406b"
-    const unit = "metric"
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit
-
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=e11166873fa5b9137dea09cd24f7406b&units=metric"
     https.get(url, function(response){
         // console.log(response.statusCode)
         // console.log(response)
 
         response.on("data",function(data){
-
             const weatherData = JSON.parse(data)
+            // console.log(weatherData)
+            // console.log(data)
+
             const temp = weatherData.main.temp
+            console.log(temp)
             const weatherDescription = weatherData.weather[0].description
             const icon = weatherData.weather[0].icon 
             const imageURL = "https://openweathermap.org/img/wn/"+icon+"@2x.png"
 
 
             res.write("<p>The weathre is currently " + weatherDescription + "</p>") 
-            res.write("<h1>The Temperture in " + query +" is " + temp + "degree Celcius</h1>")
+            res.write("<h1>The Temperture in London is " + temp + "degree Celcius</h1>")
             res.write("<img src = "+imageURL+">")
 
             res.send() 
         })
     })
-})
 
+    //res.send("Server is up and running") 
+    // we can only single res.send()  use in a app.get()  part so this is commenetd out
+})
 
 
 // Here we created our server
