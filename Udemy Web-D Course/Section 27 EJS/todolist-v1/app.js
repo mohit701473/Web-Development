e90f9d0d933755b15f0bb9070afca8ed8ff12309
mohit701473 <mohitprajapat7014@gmail.com>
2023-06-01 11:53:13 +0530

    web-d

--- Udemy Web-D Course/Section 27 EJS/todolist-v1/app.js
+++ Udemy Web-D Course/Section 27 EJS/todolist-v1/app.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs')
 
 
 let items = ["Buy Food", "Coock Food", "Eat Food"]
+let workItems = []
 
 app.get("/", function(req, res){
 
@@ -20,18 +21,29 @@ app.get("/", function(req, res){
         day: "numeric" ,
         month: "long"
     }
-    
+
     let day = today.toLocaleDateString("en-US", options) 
 
-    res.render("list", {kindOfDay: day, newListItems: items})
+    res.render("list", {listTitle: day, newListItems: items})
 })
 
 app.post("/", function(req, res){
+    
     let item = req.body.newItem
-    items.push(item)
 
-    res.redirect("/")
+    if(req.body.list === "Work"){
+        workItems.push(item)
+        res.redirect("/work")
+    }else {
+        items.push(item)
+        res.redirect("/")
+    }
 })
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+})
+
 
 app.listen(3000, function(){
     console.log("server is running on port 3000")
